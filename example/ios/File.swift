4cff9d372ab2b6debcb25110e5db5d80a490651e
reizha <reizha@importir.co>
2021-06-13 20:06:14 +0700

    message

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  DoubleClickExample
-//
-
-import Foundation
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,45 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
-};
+import React, { useEffect } from 'react';
+import { StyleProp, TouchableOpacity, ViewStyle } from 'react-native';
+export interface data {
+  singleTap: Function;
+  doubleTap: Function;
+  children: JSX.Element;
+  delay: Number;
+  style: StyleProp<ViewStyle>;
+}
+export default function DoubleClick(data: data) {
+  let delayTime = data.delay ? data.delay : 300;
+  let firstPress = true;
+  let lastTime: number = new Date().getTime();
+  let timer: number = 0;
+  const singlePress = (now: number) => {
+    firstPress = false;
+    timer = setTimeout(() => {
+      data.singleTap ? data.singleTap() : null;
+      firstPress = true;
+    }, delayTime);
+    lastTime = now;
+  };
+  const doublePress = (now: number) => {
+    console.log(lastTime);
+    if (now - lastTime < delayTime) {
+      timer && clearTimeout(timer);
+      data.doubleTap ? data.doubleTap() : null;
+      firstPress = true;
+    }
+  };
+  const _onPress = () => {
+    let now: number = new Date().getTime();
+    firstPress ? singlePress(now) : doublePress(now);
+  };
+  useEffect(() => {
+    return () => {
+      timer && clearTimeout(timer);
+    };
+  }, [timer]);
+  return (
+    <TouchableOpacity onPress={_onPress} style={data.style}>
+      {data.children}
+    </TouchableOpacity>
+  );
+}
